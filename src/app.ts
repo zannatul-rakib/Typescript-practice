8fdb5f2d1db125f2b63de24fac6791d1dc784162
zrakib-dev <dev.zrakib@gmail.com>
2023-04-27 22:09:20 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,61 @@
+// // Function Type
+// let myFunc: Function;
+// myFunc = () => {
+//   console.log("hello");
+// };
+// //function parameter
+// const greetings = (a: string, b: string, c?: string) => {
+//   return `Hello ${a} & ${b}`;
+// };
+// console.log(greetings("h", "B"));
+
+// const malbro = () => {
+//   return "hello";
+// };
+// console.log(malbro());
+
+// dynamic type or any type
+// let a: any;
+// let b: any[] = [];
+// let c: {
+//   name: any,
+//   age: any
+// }
+
+// Explicit and Union type
+// let a: string | number;
+// let b: (string | number)[] = [];
+// let c: {
+//   name: string;
+//   age: number;
+//   adult: boolean;
+// };
+
+// function multiply(a : number, b : number) {
+//   return a * b;
+// }
+// console.log(multiply("hello", 38));
+
+// let hello;
+// console.log(hello);
+// hello = "Shakib";
+// hello = 73;
+// console.log(hello);
+
+// let playerName = "rakib";
+// let age = 45;
+// age = "kksjd";
+// console.log(age)
+// let playerName = "Kishor";
+// console.log(playerName);
+// playerName = 35;
+// console.log(playerName)
+
+// console.log("hello");
+// const country = "I love Bangladesh";
+// console.log(country);
+
+// const myFunc = () => {
+//   console.log("there is function");
+// };
+// myFunc();
